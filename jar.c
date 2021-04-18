27fa37263036d9b1799d1b13fb261c7e136f500d
Nivisonu23 <60925119+Nivisonu23@users.noreply.github.com>
2021-04-18 20:07:30 +0530

    Update jar.c

--- jar.c
+++ jar.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 int main()
 {
-	int n,a[20],i,b[10],c;
+	int n,a[100],i,s,c;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
-		scanf("%d",&a[i]);
+	scanf("%d",&a[i]);
+	int b[n/2];
 	for(i=0;i<n;i+=2)
 	{
 		b[i]=a[i]+a[i+1];
+		printf("%d ",b[i]);
+		s=b[0];
+		if(b[i]<s)
+			c=i;
 	}
-	int s=sizeof(b)/sizeof(b[0]);
-	for(i=0;i<s;i++)
+	s=b[0];
+	i=0;
+	while(i<n/2)
 	{
-		if(b[i]<b[i+1])
-			c=i+1;
+		if(b[i]<s)
+			c=i;
+		i++;
 	}
-	printf("%d",c);
+	printf("\nJar %d ",c);
 	return 0;
 }
